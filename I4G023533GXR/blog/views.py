1954f498284a22158226e5c266e5ab9ea5f73e4e
Ayomide0123 <oyetimehin31@gmail.com>
2022-06-23 12:13:58 +0100

    New changes
    Django CRUD changes.

---
+++ I4G023533GXR/blog/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from I4G023533GXR.blog.models import Post
+from django.views import generic
+
+# Create your views here.
+
+
+class PostListView(generic.ListView):
+    model = Post
+
+
+class PostCreateView(generic.CreateView):
+    model = Post
+    fields: "__all__"
+    success_url: reverse_lazy("blog:all")
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+
+
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields: "__all__"
+    success_url: reverse_lazy("blog:all")
+
+
+class PostDeleteView(generic.DeleteView):
+    model = Post
+    fields: "__all__"
+    success_url: reverse_lazy("blog:all")
